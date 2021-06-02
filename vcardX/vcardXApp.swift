ed39cfbb2165cc5b4f05aca89611c3475035f7c3
odyvankinoby <nicolasott@gmail.com>
2021-06-02 09:31:48 +0200

    Initial Commit

---
+++ vcardX/vcardXApp.swift
@@ -0,0 +1,17 @@
+//
+//  vcardXApp.swift
+//  vcardX
+//
+//  Created by Nicolas Ott on 02.06.21.
+//
+
+import SwiftUI
+
+@main
+struct vcardXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
