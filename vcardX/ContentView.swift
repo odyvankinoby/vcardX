e31c12b611591b7efda3d2af0626b6e25200b912
odyvankinoby <nicolasott@gmail.com>
2021-06-02 17:35:37 +0200

    First commit for Version 0.1.1

--- vcardX/ContentView.swift
+++ vcardX/ContentView.swift
@@ -6,16 +6,120 @@
 //
 
 import SwiftUI
+import CoreImage.CIFilterBuiltins
 
 struct ContentView: View {
+    
+    @Binding var tabSelected: Int
+    @ObservedObject var settings: UserSettings
+    
+    @State private var name = "Nicolas Ott"
+    @State private var emailAddress = "mail@nicolasott.de"
+    let context = CIContext()
+    let filter = CIFilter.qrCodeGenerator()
+    
+    @State var selector: Int = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                HStack {
+                    Picker(selection: $selector, label: Text("")) {
+                        Text("Business").tag(0)
+                        Text("Private").tag(1)
+                    }.pickerStyle(SegmentedPickerStyle()).labelsHidden()
+                }.padding(10)
+            
+            
+                
+                if selector == 0 {
+                    
+                    
+                    TextField("Name", text: $name)
+                        .textContentType(.name)
+                        .font(.title)
+                        .padding(.horizontal)
+                    
+                    TextField("Email address", text: $emailAddress)
+                        .textContentType(.emailAddress)
+                        .font(.title)
+                        .padding([.horizontal, .bottom])
+                    Spacer()
+                    Image(uiImage: generateQRCode(from: "\(name)\n\(emailAddress)"))
+                        .resizable()
+                        .interpolation(.none)
+                        .scaledToFit()
+                        .frame(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        .padding()
+                    
+                }
+                else if selector == 1 {
+                    
+                    TextField("Name", text: $name)
+                        .textContentType(.name)
+                        .font(.title)
+                        .padding(.horizontal)
+                    
+                    TextField("Email address", text: $emailAddress)
+                        .textContentType(.emailAddress)
+                        .font(.title)
+                        .padding([.horizontal, .bottom])
+                    Spacer()
+                    Image(uiImage: generateQRCode(from: "\(name)\n\(emailAddress)"))
+                        .resizable()
+                        .interpolation(.none)
+                        .scaledToFit()
+                        .frame(width: 200, height: 200, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    
+                }
+                Spacer()
+                Text("Handmade with ❤️ in Munich.")
+                    .font(.caption)
+                    .padding([.horizontal, .bottom])
+               
+                
+            }
+            .background(Color("primeInverted"))
+            .toolbar {
+                ToolbarItemGroup(placement: .navigationBarLeading) {
+                    HStack {
+                        Button(action: {
+                            self.tabSelected = 2
+                        }) {
+                            Image(systemName: "gearshape").foregroundColor(Color("primeInverted"))
+                        }
+                    }
+                }
+                ToolbarItemGroup(placement: .navigationBarTrailing) {
+                    HStack {
+                        Button(action: {
+                            self.edit()
+                        }) {
+                            Image(systemName: "pencil.tip.crop.circle")
+                                .foregroundColor(Color("primeInverted"))
+                        }
+                    }
+                }
+            }
+            .navigationBarTitle("Your vCard", displayMode: .automatic).allowsTightening(true)
+        }
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    func generateQRCode(from string: String) -> UIImage {
+        let data = Data(string.utf8)
+        filter.setValue(data, forKey: "inputMessage")
+        
+        if let outputImage = filter.outputImage {
+            if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+                return UIImage(cgImage: cgimg)
+            }
+        }
+        
+        return UIImage(systemName: "xmark.circle") ?? UIImage()
+        
+    }
+    
+    func edit() {
+        
     }
 }
--- vcardX/vcardXApp.swift
+++ vcardX/vcardXApp.swift
@@ -9,9 +9,52 @@ import SwiftUI
 
 @main
 struct vcardXApp: App {
+    
+    let coloredNavAppearance = UINavigationBarAppearance()
+    
+    init() {
+        
+        // NavifgationView
+        coloredNavAppearance.configureWithOpaqueBackground()
+        coloredNavAppearance.backgroundColor = UIColor(Color("prime"))
+        coloredNavAppearance.titleTextAttributes = [.foregroundColor: UIColor(Color("primeInverted"))]
+        coloredNavAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor(Color("primeInverted"))]
+        coloredNavAppearance.backButtonAppearance.normal.titleTextAttributes = [.foregroundColor: UIColor(Color("primeInverted"))]
+        
+        UINavigationBar.appearance().standardAppearance = coloredNavAppearance
+        UINavigationBar.appearance().barTintColor = UIColor(Color("primeInverted"))
+        UINavigationBar.appearance().scrollEdgeAppearance = coloredNavAppearance
+        
+        //UITabBar.appearance().backgroundColor = UIColor.systemBackground
+        
+        // Segmented Picker
+        UISegmentedControl.appearance().selectedSegmentTintColor = UIColor(named: "prime")
+        UISegmentedControl.appearance().setTitleTextAttributes([.foregroundColor: UIColor.white], for: .selected)
+        UISegmentedControl.appearance().setTitleTextAttributes([.foregroundColor: UIColor(named: "prime") ?? Color.black], for: .normal)
+    }
+    
+    @State var tabSelected = 0
+    @ObservedObject var settings = UserSettings()
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TabView(selection: $tabSelected) {
+                ContentView(tabSelected: $tabSelected, settings: settings)
+                    .tabItem {
+                        Image(systemName: tabSelected == 0 ? "qrcode.viewfinder" : "qrcode")
+                        Text("vCard")
+                    }.tag(0)
+                ContactsView(tabSelected: $tabSelected, settings: settings)
+                    .tabItem {
+                        Image(systemName: tabSelected == 1 ? "person.fill.viewfinder" : "person.fill")
+                        Text("Contacts")
+                    }.tag(1)
+                SettingsView(tabSelected: $tabSelected, settings: settings) 
+                    .tabItem {
+                        Image(systemName: tabSelected == 2 ? "gearshape.fill" : "gearshape")
+                        Text("Settings")
+                    }.tag(2)
+            }.accentColor(Color("primeInverted"))
         }
     }
 }
