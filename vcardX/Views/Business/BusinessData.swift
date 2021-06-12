6e2f47072266c8bdf648d97b5d049cd55e0a59d4
odyvankinoby <nicolasott@gmail.com>
2021-06-12 19:55:42 +0200

    1.1.2 update text added

--- vcardX/Views/Business/BusinessData.swift
+++ vcardX/Views/Business/BusinessData.swift
@@ -15,32 +15,52 @@ struct BusinessData: View {
         HStack {
             VStack(alignment: .leading, spacing: 10) {
                 Group {
-                    Text(loc_mobile).font(.caption2).foregroundColor(.prime)
-                    Text(settings.mobileBusiness).font(.subheadline).foregroundColor(.prime).bold()
+                    if settings.mobileBusiness != "" {
+                        Text(loc_mobile).font(.caption2).foregroundColor(.prime)
+                        Text(settings.mobileBusiness).font(.subheadline).foregroundColor(.prime).bold()
+                    }
+                    if settings.landlineBusiness != "" {
                     Text(loc_landline).font(.caption2).foregroundColor(.prime)
                     Text(settings.landlineBusiness).font(.subheadline).foregroundColor(.prime).bold()
+                    }
                 }
                 Divider()
                 Group {
+                    if settings.emailBusiness != "" {
                     Text(loc_email_business).font(.caption2).foregroundColor(.prime)
                     Text(settings.emailBusiness).font(.subheadline).foregroundColor(.prime).bold()
+                    }
+                    if settings.emailBusinessOther != "" {
                     Text(loc_email_other).font(.caption2).foregroundColor(.prime)
                     Text(settings.emailBusinessOther).font(.subheadline).foregroundColor(.prime).bold()
+                    }
+                    if settings.wwwBusiness != "" {
                     Text(loc_www).font(.caption2).foregroundColor(.prime)
                     Text(settings.wwwBusiness).font(.subheadline).foregroundColor(.prime).bold()
+                    }
                 }
                 Divider()
                 Group {
+                    if settings.street1Business != "" {
                     Text(loc_street).font(.caption2).foregroundColor(.prime)
                     Text(settings.street1Business).font(.subheadline).foregroundColor(.prime).bold()
+                    }
+                    if settings.street2Business != "" {
                     Text(loc_street2).font(.caption2).foregroundColor(.prime)
                     Text(settings.street2Business).font(.subheadline).foregroundColor(.prime).bold()
+                    }
+                    if settings.zipBusiness != "" {
                     Text(loc_zip).font(.caption2).foregroundColor(.prime)
                     Text(settings.zipBusiness).font(.subheadline).foregroundColor(.prime).bold()
+                    }
+                    if settings.cityBusiness != "" {
                     Text(loc_city).font(.caption2).foregroundColor(.prime)
                     Text(settings.cityBusiness).font(.subheadline).foregroundColor(.prime).bold()
+                    }
+                    if settings.countryBusiness != "" {
                     Text(loc_country).font(.caption2).foregroundColor(.prime)
                     Text(settings.countryBusiness).font(.subheadline).foregroundColor(.prime).bold()
+                    }
                 }
             }.padding()
             Spacer()
